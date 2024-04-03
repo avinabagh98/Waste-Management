fc186f160378784d4328bc9366e67708b97a060d
Avinaba Ghosh <75163712+avinabagh98@users.noreply.github.com>
2024-04-03 12:27:03 +0530

    CSS updated v2.0

--- waste_management/components/role-lang/Buttongroup.js
+++ waste_management/components/role-lang/Buttongroup.js
@@ -11,7 +11,7 @@ export default function Buttongroup({ handleUsertypeBtn }) {
           <Col xs={6} className={styles.btnCol}>
             {/* Supervisor card */}
             <span
-              className={styles.card}
+              className={styles.card_supervisor}
               name="supervisor"
               id="1"
               onClick={(e) => handleUsertypeBtn(e)}
@@ -22,14 +22,14 @@ export default function Buttongroup({ handleUsertypeBtn }) {
                   src="/images/supervisor.png"
                   alt="supervisor-img"
                 />
-                <div className={styles.cardContent}>
+                <div className={styles.cardContent_supervisor}>
                   <p className={styles.cardText}>Supervisor</p>
                 </div>
               </a>
             </span>
             {/* Field staff card */}
             <span
-              className={styles.card}
+              className={styles.card_fieldstaff}
               name="field-staff"
               id="2"
               onClick={(e) => handleUsertypeBtn(e)}
@@ -40,14 +40,14 @@ export default function Buttongroup({ handleUsertypeBtn }) {
                   src="/images/field_staff.png"
                   alt="field-staff img"
                 />
-                <div className={styles.cardContent}>
+                <div className={styles.cardContent_fieldstaff}>
                   <p className={styles.cardText}>Field Staff</p>
                 </div>
               </a>
             </span>
             {/* Waste collector card */}
             <span
-              className={styles.card}
+              className={styles.card_wastecollector}
               name="waste-collector"
               id="3"
               onClick={(e) => handleUsertypeBtn(e)}
@@ -58,7 +58,7 @@ export default function Buttongroup({ handleUsertypeBtn }) {
                   src="/images/waste_collector.png"
                   alt="waste-collector img"
                 />
-                <div className={styles.cardContent}>
+                <div className={styles.cardContent_wastecollector}>
                   <p className={styles.cardText}>Waste Collector</p>
                 </div>
               </a>
